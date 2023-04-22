3fc39cc3e0c81ba5bfb556c51ebb89639d4aa884
Lanze09 <104864756+Lanze09@users.noreply.github.com>
2023-04-22 19:23:36 +0800

    Update Touchless_Pet_Feeder_Sketch.ino
    buttons are installed working as sensors

--- Sketches For the Program/Touchless_Pet_Feeder_Sketch.ino
+++ Sketches For the Program/Touchless_Pet_Feeder_Sketch.ino
@@ -1,13 +1,17 @@
 /*  Touchless Pet Feeder  */
 
+//REMOVE THIS~~~~~~~~~  #include <stdlib.h>
+//REMOVE THIS~~~~~~~~~  #include <stdio.h>
+
 // Headers to allow use of Wire and RTClib Libraries
 #include <Wire.h> // used for I2C communication with the RTC module
 #include <RTClib.h> // provides functions for accessing and manipulating the RTC module
 #include <Ds1302.h> // for DS1302 RTC Chip
-// #include <Serial.h> //not working for some reason
+//REMOVE THIS~~~~~~~~~  #include <Serial.h> //not working for some reason
 #include <LiquidCrystal.h> // allows usage of LiquidCrystal library, which is used to interface with the LCD display
+#include <Servo.h>
 
-//*******REMOVE_ME RTC_DS1302 rtc; // creates an instance of the RTC_DS1302 class, which is used to access the RTC module
+//*******REMOVE_ME-- RTC_DS1302 rtc; // creates an instance of the RTC_DS1302 class, which is used to access the RTC module
 
 // Essential Variables
 int DispCount = 0; // Food dispensing frequency counter
@@ -18,6 +22,7 @@ int PTime = 0; // Time checked for puppy
 int Timer = 0; // For dispense interval (The pause before next dispense)
 int Age = 0; // Switch for dog age (0=Puppy ; 1=Adult/Senior)
 
+
 // Developer Variables
 const int DispensePin = 2;
 const int AgePin = 4;
@@ -26,9 +31,9 @@ const int HumanPin = 8;
 const int PowerPin = 13;
 const int LowbatINPPin = 11; //set up the low battery threshold
 const int LowBatOUTPin = 12;
-int HumanSensor = 0;
+bool Detects = 0;
 
-Serial.begin(9600);
+//REMOVE THIS~~~~~~~~~  Serial.begin(9600);
 
 
 void setup() {
@@ -42,19 +47,30 @@ pinMode (PowerPin, OUTPUT);
 pinMode (LowbatINPPin, INPUT);
 pinMode (LowBatOUTPin, OUTPUT);
 
+
+
 }
 
 void loop() {
   // This shit runs over and over and over again no cap
 
-digitalWrite(PowerPin, HIGH);
+//*******REMOVE_ME-- digitalWrite(PowerPin, HIGH);
+
+digitalWrite(PowerPin, LOW);
 
 
-HumanSensor = digitalRead(HumanPin);
-if (HumanPin == HIGH){
-;
-delay(5000);
-
+if (digitalRead(HumanPin) == 1){
+  digitalWrite(PowerPin, HIGH);
+  delay(1000);
+  digitalWrite(PowerPin, LOW);
 }
+
+
+if (digitalRead(DogPin) == 1){
+  digitalWrite(PowerPin, HIGH);
+  delay(1000);
+  digitalWrite(PowerPin, LOW);
+}
+
 
 }
