c6d76023c3afd93b209db283f74d747f7b26d8e4
Lanze09 <104864756+Lanze09@users.noreply.github.com>
2023-04-22 23:24:31 +0800

    Updated Conditions for Dog Sensor
    Added certain conditions for the dog sensor including daytime / age / timer / Dispensed amount

--- Sketches For the Program/Touchless_Pet_Feeder_Sketch.ino
+++ Sketches For the Program/Touchless_Pet_Feeder_Sketch.ino
@@ -1,8 +1,7 @@
 /*  Touchless Pet Feeder  */
 
-//REMOVE THIS~~~~~~~~~  #include <stdlib.h>
-//REMOVE THIS~~~~~~~~~  #include <stdio.h>
-
+#include <stdlib.h>
+#include <stdio.h>
 // Headers to allow use of Wire and RTClib Libraries
 #include <Wire.h> // used for I2C communication with the RTC module
 #include <RTClib.h> // provides functions for accessing and manipulating the RTC module
@@ -13,14 +12,17 @@
 
 //*******REMOVE_ME-- RTC_DS1302 rtc; // creates an instance of the RTC_DS1302 class, which is used to access the RTC module
 
+
+
 // Essential Variables
 int DispCount = 0; // Food dispensing frequency counter
 int MaxDisp = 0; // Maximum dispense per feeding interval
-int CTime = 0; // Current time / Last identified "Time"
-int ATime = 0; // Time checked for adult / senior
-int PTime = 0; // Time checked for puppy
+int CTime = 0; //TENTATIVE- Current time / Last identified "Time"
+int ATime = 0; // Time checker for adult / senior
+int PTime = 0; // Time checker for puppy
 int Timer = 0; // For dispense interval (The pause before next dispense)
-int Age = 0; // Switch for dog age (0=Puppy ; 1=Adult/Senior)
+bool Adult = false; // Switch for dog age (false=Puppy ; true=Adult/Senior)
+
 
 
 // Developer Variables
@@ -28,7 +30,8 @@ const int DispensePin = 2;
 const int AgePin = 4;
 const int DogPin = 7;
 const int HumanPin = 8;
-const int PowerPin = 13;
+const int DispSignal = 13;
+const int PowerPin = 12; 
 const int LowbatINPPin = 11; //set up the low battery threshold
 const int LowBatOUTPin = 12;
 bool Detects = 0;
@@ -43,34 +46,87 @@ pinMode (DispensePin, OUTPUT);
 pinMode (AgePin, INPUT);
 pinMode (DogPin, INPUT);
 pinMode (HumanPin, INPUT);
+pinMode (DispSignal, OUTPUT);
 pinMode (PowerPin, OUTPUT);
 pinMode (LowbatINPPin, INPUT);
 pinMode (LowBatOUTPin, OUTPUT);
-
-
-
 }
+
 
 void loop() {
   // This shit runs over and over and over again no cap
 
 //*******REMOVE_ME-- digitalWrite(PowerPin, HIGH);
 
-digitalWrite(PowerPin, LOW);
+digitalWrite(DispSignal, LOW);
 
 
-if (digitalRead(HumanPin) == 1){
-  digitalWrite(PowerPin, HIGH);
+if (digitalRead(HumanPin) == 1)
+{
+  digitalWrite(DispSignal, HIGH);
+  // (Add Command) Set command to open dispenser via servo motor
   delay(1000);
-  digitalWrite(PowerPin, LOW);
+  digitalWrite(DispSignal, LOW);
+  DispCount++;
+  // (Add Command) Set command to close dispenser via servo motor
 }
 
 
-if (digitalRead(DogPin) == 1){
-  digitalWrite(PowerPin, HIGH);
-  delay(1000);
-  digitalWrite(PowerPin, LOW);
-}
+if (digitalRead(DogPin) == 1)
+{
+ if (Timer <= 0)
+  {
+   if (Adult == true) //DESC -- If Adult
+   {
+     ATime = 0; //TENTATIVE- Set this command to change ATime to actual current time from RTC Module
+     MaxDisp = 10; //TENTATIVE- Set this command to change the average Maximum dispense to meet the enough amount of food for this meal
+     
+     if (ATime != 3)
+     {
+      if (CTime != ATime)
+      {
+       CTime = ATime;
+       DispCount = 0;
+      }
+      
+      if (DispCount < MaxDisp)
+          {
+            digitalWrite(DispSignal, HIGH);
+            // (Add Command) Set command to open dispenser via servo motor
+            delay(1000);
+            digitalWrite(DispSignal, LOW);
+            // (Add Command) Set command to close dispenser via servo motor
+            DispCount++;
+            // (Add Command) Set new Timer via RTC Module
+          }       
+          
+      }
+    }
 
-
+    else //DESC-- not adult
+      {
+        PTime = 0; //TENTATIVE-- Set this command to change PTime to actual current time from RTC Module
+        MaxDisp = 8; //TENTATIVE-- Set this command to change the average Maximum dispense to meet the enough amount of food for this meal
+        if (PTime != 4)
+        {
+          if (CTime != PTime) //DESC-- Daytime changes. Will reset the DispCount to zero
+          {
+          CTime = PTime;
+          DispCount = 0;
+          }
+          
+          if (DispCount < MaxDisp)
+          {
+            digitalWrite(DispSignal, HIGH);
+            // (Add Command) Set command to open dispenser via servo motor
+            delay(1000);
+            digitalWrite(DispSignal, LOW);
+            // (Add Command) Set command to close dispenser via servo motor
+            DispCount++;
+            // (Add Command) Set new Timer via RTC Module  
+          }
+        }
+      }      
+    }    
+  }
 }
